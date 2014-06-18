a95dc1bd9d21cfd28a6bb7a2222e9f116db64394
Francesco Montefoschi <francesco.monte@gmail.com>
2014-06-18 09:54:32 +0200

    fix stop led

--- scripts/stop-led.sh
+++ scripts/stop-led.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kill `ps u |grep blink\-led\.php |grep -v grep |awk '{print $2}'`
+kill `ps aux |grep blink\-led\.php |grep -v grep |awk '{print $2}'`
 echo 0 > /sys/class/gpio/gpio25/value
