4af9b5807ef97642edf1d4c74225820d24c28ad4
Francesco Montefoschi <francesco.monte@gmail.com>
2014-11-17 19:52:32 +0100

    webcam udoo

--- scripts/shotimage.sh
+++ scripts/shotimage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-fswebcam -d /dev/video0 -r 640x480 --no-banner ../public/webcam.jpg
+if [ -f "/etc/udoo-config.conf" ]
+then
+        WEBCAM="/dev/v4l/by-id/usb-Vimicro_Vimicro_USB_Camera__Altair_-video-index0"
+else
+        WEBCAM="/dev/video0"
+fi
+
+fswebcam -d $WEBCAM -r 640x480 --no-banner ../public/webcam.jpg
